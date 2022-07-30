4077e1d373acdea3c10702d9d77db5f479512c00
Bruno Faustino Cecilio <49068550+brunocecilio@users.noreply.github.com>
2022-07-30 00:03:07 -0300

    feat: generic database class
    finishes save method
    change method name from "search" to "get"
    implements listAll and listFilter methods
    implements delete method
    now the database file is created if it not exist
    now creates a temp file before actually overwriting the database
    
    .gitignore: ignore database files

--- src/br/com/letscode/database/Database.java
+++ src/br/com/letscode/database/Database.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,8 +12,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
 
-public class Database<E extends Serializable, K extends Serializable> {
+import br.com.letscode.exception.FalhaInicializandoBancoDeDadosError;
+
+public class Database<K extends Serializable, E extends Serializable> {
     final Class<E> DAOClass;
 
     static final String DATABASE_PATH = "./db";
@@ -20,47 +26,111 @@ public class Database<E extends Serializable, K extends Serializable> {
     static final String DATABASE_SEPARATOR = ",";
 
     public Database(Class<E> DAOClass) {
+        final String EXCEPTION_MESSAGE = "Não foi possível criar o arquivo de dados.";
+
         this.DAOClass = DAOClass;
+        if (!new File(getDataBaseTablePath()).isFile()) {
+            try {
+                File directory = new File(DATABASE_PATH);
+                if (!directory.exists()) {
+                    directory.mkdir();
+                }
+                new File(getDataBaseTablePath()).createNewFile();
+            } catch (IOException e) {
+                throw new FalhaInicializandoBancoDeDadosError(EXCEPTION_MESSAGE, e);
+            }
+        }
     }
 
     public String getDataBaseTablePath() {
         return DATABASE_PATH + "/" + DAOClass.getSimpleName().toUpperCase() + "." + DATABASE_FILE_EXTENSION;
     }
 
-    public void save(K key, E entity) throws IOException {
-        final String IO_EXCEPTION_MESSAGE = "Não foi possível salvar a entidade.";
+    @SuppressWarnings("unchecked")
+    private void editLine(K key, String newLine, boolean addLineIfNotFound) throws IOException, ClassNotFoundException {
+        final String EXCEPTION_MESSAGE = "Não foi possível editar a tabela de dados.";
 
         try {
-            StringBuilder dbLine = new StringBuilder();
+            BufferedReader databaseTableFile = new BufferedReader(new FileReader(getDataBaseTablePath()));
+            StringBuilder inputBuilder = new StringBuilder();
+            String line;
+            boolean found = false;
+            while ((line = databaseTableFile.readLine()) != null) {
+                if (line.length() == 0) {
+                    continue;
+                }
 
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(baos);
-            oos.writeObject(key);
-            dbLine.append(Base64.getEncoder().encodeToString(baos.toByteArray()));
-            baos.close();
-            oos.close();
+                byte[] idData = Base64.getDecoder().decode(line.split(DATABASE_SEPARATOR)[0]);
+                ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(idData));
+                K entityId = (K) ois.readObject();
+                ois.close();
 
-            baos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(baos);
-            oos.writeObject(entity);
-            dbLine.append(DATABASE_SEPARATOR);
-            dbLine.append(Base64.getEncoder().encodeToString(baos.toByteArray()));
-            dbLine.append(System.lineSeparator());
-            baos.close();
-            oos.close();
+                if (entityId.equals(key)) {
+                    line = newLine;
+                    found = true;
+                }
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(this.getDataBaseTablePath(), true));
-            writer.write(dbLine.toString());
+                if (line != null) {
+                    inputBuilder.append(line + System.lineSeparator());
+                }
+            }
+            databaseTableFile.close();
+
+            if (addLineIfNotFound && !found) {
+                inputBuilder.append(newLine);
+            }
+
+            if (inputBuilder.toString().endsWith(System.lineSeparator().repeat(2))) {
+                inputBuilder.delete(inputBuilder.length() - System.lineSeparator().length(), inputBuilder.length());
+            }
+
+            String prefix = DAOClass.getSimpleName().toUpperCase() + "_";
+            String suffix = ".csv.tmp";
+            File newFile = File.createTempFile(prefix, suffix, new File(DATABASE_PATH));
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
+            writer.write(inputBuilder.toString());
             writer.close();
+
+            new File(getDataBaseTablePath()).delete();
+            newFile.renameTo(new File(getDataBaseTablePath()));
+
         } catch (IOException e) {
-            throw new IOException(IO_EXCEPTION_MESSAGE, e);
+            throw new IOException(EXCEPTION_MESSAGE, e);
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundException(EXCEPTION_MESSAGE, e);
         }
     }
 
+    public void save(K key, E entity) throws IOException, ClassNotFoundException {
+        StringBuilder dbLine = new StringBuilder();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(key);
+        dbLine.append(Base64.getEncoder().encodeToString(baos.toByteArray()));
+        baos.close();
+        oos.close();
+
+        baos = new ByteArrayOutputStream();
+        oos = new ObjectOutputStream(baos);
+        oos.writeObject(entity);
+        dbLine.append(DATABASE_SEPARATOR);
+        dbLine.append(Base64.getEncoder().encodeToString(baos.toByteArray()));
+        dbLine.append(System.lineSeparator());
+        baos.close();
+        oos.close();
+
+        editLine(key, dbLine.toString(), true);
+    }
+
+    public void delete(K id) throws IOException, ClassNotFoundException {
+        editLine(id, null, false);
+    }
+
     @SuppressWarnings("unchecked")
-    public E search(K id) throws IOException, ClassNotFoundException {
-        final String IO_EXCEPTION_MESSAGE = "Não foi possível carregar a entidade.";
-        final String CLASS_NOT_FOUND_EXCEPTION_MESSAGE = "Não foi possível carregar a entidade.";
+    public E get(K id) throws IOException, ClassNotFoundException {
+        final String EXCEPTION_MESSAGE = "Não foi possível carregar a entidade.";
 
         try {
             E entity = null;
@@ -68,6 +138,10 @@ public class Database<E extends Serializable, K extends Serializable> {
             BufferedReader br = new BufferedReader(inputFile);
             String line;
             while ((line = br.readLine()) != null) {
+                if (line.length() == 0) {
+                    continue;
+                }
+
                 String[] entityDataBase64 = line.split(DATABASE_SEPARATOR);
                 byte[] idData = Base64.getDecoder().decode(entityDataBase64[0]);
                 ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(idData));
@@ -89,14 +163,54 @@ public class Database<E extends Serializable, K extends Serializable> {
 
             return entity;
         } catch (IOException e) {
-            throw new IOException(IO_EXCEPTION_MESSAGE, e);
+            throw new IOException(EXCEPTION_MESSAGE, e);
         } catch (ClassNotFoundException e) {
-            throw new ClassNotFoundException(CLASS_NOT_FOUND_EXCEPTION_MESSAGE, e);
+            throw new ClassNotFoundException(EXCEPTION_MESSAGE, e);
         }
-
     }
 
-    // public List<E> listAll(E entity) {
+    @SuppressWarnings("unchecked")
+    public Map<K, E> listFilter(Predicate<E> filter) throws IOException, ClassNotFoundException {
+        final String EXCEPTION_MESSAGE = "Não foi possível carregar a entidade.";
 
-    // }
+        Map<K, E> entities = new HashMap<>();
+
+        try {
+            FileReader inputFile = new FileReader(this.getDataBaseTablePath());
+            BufferedReader br = new BufferedReader(inputFile);
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.length() == 0) {
+                    continue;
+                }
+
+                String[] entityDataBase64 = line.split(DATABASE_SEPARATOR);
+                byte[] idData = Base64.getDecoder().decode(entityDataBase64[0]);
+                ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(idData));
+                K entityId = (K) ois.readObject();
+                ois.close();
+
+                byte[] entityData = Base64.getDecoder().decode(entityDataBase64[1]);
+                ois = new ObjectInputStream(new ByteArrayInputStream(entityData));
+                E entity = (E) ois.readObject();
+                ois.close();
+
+                if (filter.test(entity)) {
+                    entities.put(entityId, entity);
+                }
+            }
+            br.close();
+            inputFile.close();
+
+            return entities;
+        } catch (IOException e) {
+            throw new IOException(EXCEPTION_MESSAGE, e);
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundException(EXCEPTION_MESSAGE, e);
+        }
+    }
+
+    public Map<K, E> listAll() throws IOException, ClassNotFoundException {
+        return listFilter(entity -> true);
+    }
 }
